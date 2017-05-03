fa5adedd95d0fc05c419e8bab6b0cfd1916615ee
สุขพัฒน์ <cakem2050@gmail.com>
2017-05-04 02:17:30 +0700

    edit mainapp for scan package

--- java/Controller/MainApp.java
+++ java/Controller/MainApp.java
@@ -2,8 +2,12 @@ package Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("Entities")
+@EnableJpaRepositories("Repository")
 public class MainApp {
 
 	public static void main(String[] args) {
